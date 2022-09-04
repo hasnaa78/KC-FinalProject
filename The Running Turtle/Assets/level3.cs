35bb866e959fdf8451b95b9a4661293f288b4c9f
hasnaa <jgdg1649@gmail.com>
2022-09-04 03:48:31 -0700

    /

---
+++ The Running Turtle/Assets/level3.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level3 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            print("is trigger");
+            SceneManager.LoadScene(3);
+        }
+    }
+}
